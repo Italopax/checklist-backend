083f2bc390fbdfe9657c1601bc92a467fa92ea57
Italo Paixão Gomes <italopaxg@gmail.com>
2025-03-24 22:33:35 -0300

    refactor: remove unnecessary change type of database port env

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,7 @@ import { getEnv } from "../constants";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: getEnv().database.host,
-  port: Number(getEnv().database.port),
+  port: getEnv().database.port,
   username: getEnv().database.username,
   database: getEnv().database.database,
   password: getEnv().database.password,
