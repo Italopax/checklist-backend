b090a057ef33a223d29d99ac0604bfd2a328ac74
Ítalo Gomes <italopaxg@gmail.com>
2025-08-14 00:22:52 -0300

    fix: use httpOnly cookies

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -31,7 +31,7 @@ export class AuthService implements IAuthService {
       name,
       value: tokenValue,
       options: {
-        httpOnly: false,
+        httpOnly: true,
         secure: true,
         maxAge,
       },
